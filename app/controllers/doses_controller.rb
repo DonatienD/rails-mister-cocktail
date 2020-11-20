d5f6f42ae25b784cd059eb49a08ef59eb41f3845
Donatien D <ddlu.web@icloud.com>
2020-11-20 12:54:40 +0100

    New ingredient error fixed

--- app/controllers/doses_controller.rb
+++ app/controllers/doses_controller.rb
@@ -6,17 +6,15 @@ class DosesController < ApplicationController
 
   def create
     @dose = Dose.new(dose_params)
-    if @ingredient.nil?
-      render :new
-      return
-    end
-    @ingredient = Ingredient.find(params[:dose][:ingredient_id])
-    @dose.ingredient = @ingredient
-    @dose.cocktail = @cocktail
-    if @dose.save
+    if params[:dose][:ingredient_id] != ""
+      @ingredient = Ingredient.find(params[:dose][:ingredient_id])
+      @dose.ingredient = @ingredient
+      @dose.cocktail = @cocktail
+      @dose.save
       redirect_to cocktail_path(@cocktail)
     else
-      render :new
+      @dose = Dose.new
+      redirect_to new_cocktail_dose_path(@cocktail)
     end
   end
 
